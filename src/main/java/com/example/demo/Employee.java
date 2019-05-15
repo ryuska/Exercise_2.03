98d1f62e01a5ae45bcde8db8833adf6191cd51ca
ryuska <yromanova2010@gmail.com>
2019-05-15 14:13:02 -0400

    First Commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private String dob;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
